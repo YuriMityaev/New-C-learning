1771d99af150df7cfccbd5757447b801ce700c2c
YuriMityaev <eksia@bk.ru>
2024-11-16 00:31:58 +0500

    сделаны задания из семинара, остановился на 1ч14

--- seminar_1/Program.cs
+++ seminar_1/Program.cs
@@ -8,20 +8,51 @@
 // a = 2, b = 10 => нет
 // a = 9, b = -3 => да
 // a = -3, b = 9 => нет
-Console.Write("Введите число: ");
-int firstNmber = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите второе число: ");
-int secondNumber = Convert.ToInt32(Console.ReadLine());
 
-if (firstNmber == secondNumber*secondNumber)
-{
-    Console.WriteLine("Да");
-}
-else
-{
-    Console.WriteLine("Нет");
-}
 
 //Console.Write("Введите число: ");
-//int number = Convert.ToInt32(Console.ReadLine());
-//Console.WriteLine(number +10);
+//int firstNmber = Convert.ToInt32(Console.ReadLine());
+//Console.Write("Введите второе число: ");
+//int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+//if (firstNmber == secondNumber*secondNumber)
+//{
+//    Console.WriteLine("Да");
+//}
+//else
+//{
+//    Console.WriteLine("Нет");
+//}
+
+
+// Console.Write("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// УПРОЩЕННЫЙ ВАРИАНТ УКАЗАНИЯ ПЕРЕМЕННОЙ ПРИ ВЫВОДЕ СТРОКИ
+// Console.WriteLine($"{number} + 10");
+
+//Задание 2. Совместная работа
+// Семинар 1. Знакомство с языками программирования
+// Напишите программу, которая на вход принимает
+// целое число N, а на выходе показывает все целые
+// числа в промежутке от -N до N.
+// Примеры
+
+// 4 => -4, -3, -2, -1, 0, 1, 2, 3, 4
+//2 => -2, -1, 0, 1, 2
+
+Console.Write("Введите число: ");
+int number = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+// int i = -number;
+// while(i<=number)
+//{
+//System.Console.Write(i + " ");
+//i++; // <= это аналог конструкции i = i + 1
+//}
+//System.Console.WriteLine();
+
+// ОТДЕЛЬНО РЕШЕНИЕ ЧЕРЕЗ "FOR" 
+for(int i = -number; i <=number; i++)
+    System.Console.Write(i + " ");
+// если нужно сделать больше одного дейсвия необходимо 
+// прописывать через контекст {}
+
--- seminar_1/obj/Debug/net7.0/seminar_1.AssemblyInfo.cs
+++ seminar_1/obj/Debug/net7.0/seminar_1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("seminar_1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ecbb6a1adb82f5d86dc7fde45a2ddb249a33454e")]
 [assembly: System.Reflection.AssemblyProductAttribute("seminar_1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("seminar_1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
