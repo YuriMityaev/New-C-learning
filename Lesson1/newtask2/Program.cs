a647ded1b704c76088018e853a0f921db504a017
YuriMityaev <eksia@bk.ru>
2024-11-19 23:07:46 +0500

    make an structure

---
+++ Lesson1/newtask2/Program.cs
@@ -0,0 +1,40 @@
+﻿int i1 = 5;
+int i2 = 6;
+int sum = i1 + i2;
+
+Console.WriteLine(sum);
+
+
+long l1 = 6;
+long sum2 = l1 + i1;
+Console.WriteLine(sum2);
+
+double d1 = 5.5;
+double sum3 = d1 + i1;
+Console.WriteLine(sum3);
+
+int product1 = i1 * i2;
+long product2 = i1 * l1;
+double Product3 = d1 * i1;
+Console.WriteLine(product1);
+Console.WriteLine(product2);
+Console.WriteLine(Product3);
+
+int quotilent = i1 / i2;
+Console.WriteLine(quotilent);
+
+d1 = 6;
+
+double quotilent2 = i1 / d1;
+Console.WriteLine(quotilent2);
+
+int a = 5;
+int b = 44;
+int c = 8;
+int d = 8;
+
+double q1 = (a*b)/(c+d);
+Console.WriteLine(q1);
+
+int a = ;
+int b = ;
