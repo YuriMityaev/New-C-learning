286d89d42f3d70bf950321bd77d6a47d9b534bcf
YuriMityaev <eksia@bk.ru>
2024-11-19 23:44:07 +0500

    added arrangement, done first half of task 1 and ent on 25:26 sec of videolesson

--- Lesson2/task1/Program.cs
+++ Lesson2/task1/Program.cs
@@ -1,13 +1,20 @@
-﻿int[] arr = new int [5];
-arr[0] =5;
-arr[1] =6;
-arr[2] =7;
-arr[3] =3;
-arr[4] =8;
-System.Console.WriteLine(arr[0]);
-System.Console.WriteLine(arr[1]);
-System.Console.WriteLine(arr[2]);
-System.Console.WriteLine(arr[3]);
-System.Console.WriteLine(arr[4]);
+﻿//int[] arr = new int[5];
+//arr[0] = 5;
+//arr[1] = 6;
+//arr[2] = 7;
+//arr[3] = 3;
+//arr[4] = 8;
+//System.Console.WriteLine(arr[0]);
+//System.Console.WriteLine(arr[1]);
+//System.Console.WriteLine(arr[2]);
+//System.Console.WriteLine(arr[3]);
+//System.Console.WriteLine(arr[4]);
 
-System.Console.WriteLine(arr[5]);
+// System.Console.WriteLine(arr[5]);
+
+// int[] arr2 = new int[5] { 3, 5, 6, 2, 3 };
+
+// int [] arr3 = {4,12,4,3,2}
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
---
+++ Lesson2/task2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int [] arr = new int[n];
+//int i = 0;
+
+//while (i< n)
+for(int i = 0; i < n; arr[i]= i + 1)
+{
+    arr[i] = i + 1; 
+    System.Console.Write($"{arr[i]}" + " ");
+   i = i + 1;
+}
