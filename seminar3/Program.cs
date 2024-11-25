8a72e29c69bdd1f1d9157afb0d698ad06be4c7b5
Yuri <eksia@bk.ru>
2024-11-25 17:50:10 +0500

    seminar was started by me , last time - 24:00

---
+++ seminar3/Program.cs
@@ -0,0 +1,14 @@
+﻿
+// string[] names = {"Nikita", "Nastya", "Egor"};
+
+// names[0] = "New Name";
+// System.Console.WriteLine(names[0]);
+
+// Необходимо задать массив и написать программу 
+// принимающую число и проверяющая если ли число в массиве.
+
+int[]   array = new int[10];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
