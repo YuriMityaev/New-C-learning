d0d6f16eb6b2d29d100a388be78feac19109f50c
Yuri <eksia@bk.ru>
2024-11-25 16:47:38 +0500

    added foreach method in to task 4 lesson 2

---
+++ Lesson2/task3/Program.cs
@@ -0,0 +1,20 @@
+﻿int n = 5;
+int[] array = { 4, 2, 9, 6, 7 };
+int i = 0;
+int max = array[0];
+// while(i < n)
+// {
+//     if (array[i] > max)
+//     {
+//         max = array[i];
+//     }
+//     i = i +1;
+// }
+for (i = 0; i < n; i = i + 1)
+{
+    if (array[i] > max)
+    {
+        max = array[i];
+    }
+}
+System.Console.WriteLine(max);
---
+++ Lesson2/task4/Program.cs
@@ -0,0 +1,35 @@
+﻿//int n = 5;
+int[] array = { 4, 2, 9, 6, 7, 3, 34, 44};
+// int i = 0;
+int max = array[0];
+// while (i < n)
+// {
+//     if (array[i] > max)
+//     {
+//         max = array[i];
+//     }
+//     i = i + 1;
+// }
+// System.Console.WriteLine(max);
+
+
+
+// вариант 2
+// max - array[0]; - обнуление макс до 1 значения массива
+// for (int j = 0; j < n; j++)
+// {
+//     if (array[j] > max)
+//     {
+//         max = array[j];
+//     }
+// }
+
+// System.Console.WriteLine(max);
+foreach(int e in array)
+{
+    if (e>max)
+{
+    max = e;
+}
+}
+System.Console.WriteLine(max);
