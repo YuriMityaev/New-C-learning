118bec434ad2ff47c0030c8dad98c32b7cfe74e5
Yuri <eksia@bk.ru>
2024-12-05 16:21:37 +0500

    make first examle

---
+++ Lesson3/Program.cs
@@ -0,0 +1,13 @@
+﻿// начинаем работать с функциями.
+
+double CalculateFormula(int a, int b, int c, int d)
+{
+    double numinate = a * b;
+    double denomenator = c + d;
+    double result = numinate / denomenator;
+    return result;
+}
+
+double result = CalculateFormula(4, 4, 6, 6);
+
+System.Console.WriteLine(result);
