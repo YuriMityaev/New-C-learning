ee59db7845f26a4c21142b67910b2adf58d3ac95
Yuri <eksia@bk.ru>
2024-12-05 15:46:56 +0500

    save before start Lesson 3

--- seminar3/Program.cs
+++ seminar3/Program.cs
@@ -7,8 +7,138 @@
 // Необходимо задать массив и написать программу 
 // принимающую число и проверяющая если ли число в массиве.
 
-int[]   array = new int[10];
-for (int i = 0; i < array.Length; i++)
-{
-    array[i] = Convert.ToInt32(Console.ReadLine());
-}
+// Способ 2
+
+
+// int[]   array = new int[4];
+// System.Console.WriteLine("Введите значения массива: ");
+// for (int i = 0; i < array.Length; i++)
+
+// {
+//     System.Console.Write($"Введите {i}-й элемент массива:" );
+//     array[i] = Convert.ToInt32(Console.ReadLine());
+// }
+
+// for (int i = 0; i < array.Length; i++)
+// {
+//     System.Console.Write(array[i] + " " ); 
+// }
+
+// 3-й способ с рандомной генерацией + решение задачи.
+
+// System.Console.WriteLine("Введите размер массива: ");
+// int size = Convert.ToInt32(Console.ReadLine());
+// int[] array = new int[size];
+// Random rand = new Random();
+
+// for (int i = 0; i < array.Length; i++)
+// {
+//     array[i] = rand.Next(0, 10);
+// }
+
+// for (int i = 0; i < array.Length; i++)
+// {
+//     System.Console.Write(array[i] + " ");
+// }
+
+// System.Console.WriteLine("\nВведите искомое число");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// bool numberIsFinded = false;
+
+// for(int i = 0; i < array.Length; i++)
+// {
+//     if(array[i] == number)
+//     {
+//         numberIsFinded = true;
+//         break;
+//     }
+// }
+// if(numberIsFinded)
+// {
+//     System.Console.WriteLine("Да");
+// }
+// else
+// {
+//     System.Console.WriteLine("Нет");
+// }
+
+// Задача: задать массив из 10 элеменотов, заполненный в промежутке [-10;10]
+// заменим отрицательные на положительные и наоборот.
+
+// System.Console.WriteLine("Введите размер массива: ");
+// int size = Convert.ToInt32(Console.ReadLine());
+// int[] array = new int[size];
+// Random rand = new Random();
+
+// for (int i = 0; i < array.Length; i++)
+// {
+//     array[i] = rand.Next(-10, 11);
+// }
+
+// for (int i = 0; i < array.Length; i++)
+// {
+//     System.Console.Write(array[i] + " ");
+// }
+
+
+// for(int i = 0; i < array.Length; i++)
+// {
+//     array[i] *= -1;
+// }
+// System.Console.WriteLine();
+
+// задача 4. СОздать массив и умножить его первое число на последние, предполследние на второе и тд.
+// вывести визуально результат. 
+
+//  System.Console.WriteLine("Введите размер массива: ");
+// int size = Convert.ToInt32(Console.ReadLine());
+// int[] array = new int[size];
+// Random rand = new Random();
+
+// for (int i = 0; i < array.Length; i++)
+// {
+//     array[i] = rand.Next(-10, 11);
+// }
+
+// for (int i = 0; i < array.Length; i++)
+// {
+//     System.Console.Write(array[i] + " ");
+// }
+
+// int[] newArray = new int[array.Length / 2];
+
+// for(int i=0; i < newArray.Length; i++)
+// {
+//     newArray[i] = array[i] * array[array.Length - 1 - i];
+// }
+// System.Console.WriteLine();
+
+// for(int i=0; i < newArray.Length; i++)
+// {
+//     System.Console.Write(newArray[i] + " ");
+// }
+
+// Задача 5. ДАно трехнзначное число. Создайте массив фицр из числа.
+// Младший разряд расположен на 0 индексе массива. старший на 2 индексе.
+
+// int number = new Random().Next(100, 1000);
+// System.Console.WriteLine("Сгенерированное число:" + number);
+
+// int[] array = new int[3];
+
+// for(int i = 0; i < array.Length; i++)
+// {
+//     array[i] = number % 10; 
+//     number /= 10;
+// }
+
+// for (int i = 0; i < array.Length; i++)
+// {
+//     System.Console.Write(array[i] + " ");
+// }
+
+// Генерация вещественного числа.
+// Random rand = new Random();
+// double number = Math.Round(rand.Next(0, 10) + rand.NextDouble(), 3);
+// System.Console.WriteLine("Сгенерированное число: " + number);
--- seminar3/obj/Debug/net9.0/seminar3.AssemblyInfo.cs
+++ seminar3/obj/Debug/net9.0/seminar3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("seminar3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0d6f16eb6b2d29d100a388be78feac19109f50c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a72e29c69bdd1f1d9157afb0d698ad06be4c7b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("seminar3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("seminar3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
